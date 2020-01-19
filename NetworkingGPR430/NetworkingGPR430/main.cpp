e27de3ec3a1390149f53dfc7532622f2e558b0cf
Ben Strong <benjamin.strong@mymail.champlain.edu>
2020-01-19 14:36:22 -0500

    My RakNet is not working, but code pasted from tutorial 3 and message has been changed.  Needs to be tested and message loop needs to be created still.

--- NetworkingGPR430/NetworkingGPR430/main.cpp
+++ NetworkingGPR430/NetworkingGPR430/main.cpp
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
+
+//RakNet Includes
 #include "RakNet/RakPeerInterface.h"
+#include "RakNet/MessageIdentifiers.h"
+#include "RakNet/BitStream.h"
+#include "RakNet/RakNetTypes.h"
 
 unsigned int maxClients = 10;
 unsigned short serverPort = 60000;
+
+enum GameMessages
+{
+	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1
+};
 
 int main(void)
 {
 	char str[512];
 	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	RakNet::Packet* packet;
 	bool isServer;
 
 	printf("(C) or (S)erver?\n");
@@ -26,6 +39,89 @@ int main(void)
 
 
 	// TODO - Add code body here
+	if (isServer) 
+	{
+		//Set up server things
+		peer->SetMaximumIncomingConnections(maxClients);
+		std::cout << "Starting Server\n";
+	}
+	else 
+	{
+		printf("Enter server IP or hit enter for 127.0.0.1\n");
+		gets(str);
+		if (str[0] == 0) {
+			strcpy(str, "127.0.0.1");
+		}
+		printf("Starting the client.\n");
+		peer->Connect(str, serverPort, 0, 0);
+	}
+
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+			{
+				printf("Our connection request has been accepted.\n");
+
+				// Use a BitStream to write a custom user message
+				// Bitstreams are easier to use than sending casted structures, and handle endian swapping automatically
+				RakNet::BitStream bsOut;
+				bsOut.Write((RakNet::MessageID)ID_GAME_MESSAGE_1);
+				bsOut.Write("Ben and Adam's Custom Message");
+				peer->Send(&bsOut, HIGH_PRIORITY, RELIABLE_ORDERED, 0, packet->systemAddress, false);
+			}
+			break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				if (isServer) {
+					printf("A client has disconnected.\n");
+				}
+				else {
+					printf("We have been disconnected.\n");
+				}
+				break;
+			case ID_CONNECTION_LOST:
+				if (isServer) {
+					printf("A client lost the connection.\n");
+				}
+				else {
+					printf("Connection lost.\n");
+				}
+				break;
+
+			case ID_GAME_MESSAGE_1:
+			{
+				RakNet::RakString rs;
+				RakNet::BitStream bsIn(packet->data, packet->length, false);
+				bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+				bsIn.Read(rs);
+				printf("%s\n", rs.C_String());
+			}
+			break;
+
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
+	}
 
 	RakNet::RakPeerInterface::DestroyInstance(peer);
 
