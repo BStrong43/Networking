67a4d65a52eb9c3530a8a84d52c99ac40622137d
Ben Strong <benjamin.strong@mymail.champlain.edu>
2020-02-09 19:47:32 -0500

    Created client subclass of peer

---
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3Client.cpp
@@ -0,0 +1,87 @@
+#include "a3Client.h"
+#include "a3NetworkDemoState.h"
+#include "animal3D-A3DG/a3graphics/a3_TextRenderer.h"
+#include <GL/glew.h>
+#include <cstdio>
+
+a3Client::a3Client()
+	:a3Peer()
+{
+
+}
+
+void a3Client::init(a3_DemoState* pDemoState)
+{
+
+}
+
+void a3Client::cleanup(a3_DemoState* pDemoState)
+{
+
+}
+
+void a3Client::input(a3_DemoState* pDemoState)
+{
+	// C
+	if (pDemoState->keyboardInput.getKeyPress(pDemoState->keyboard, 'C'))
+	{
+		// is not already chatting, start chatting and reset typing buffer
+		if (!isChatting)
+		{
+			isChatting = true;
+			pDemoState->keyboardInput.resetTypingSession();
+		}
+	}
+	// Enter
+	else if (pDemoState->keyboardInput.getKeyPress(pDemoState->keyboard, a3key_enter))
+	{
+		// submit chat and stop chatting if chatting
+		if (isChatting)
+		{
+			isChatting = false;
+		}
+	}
+
+	if (isChatting)
+	{
+		pDemoState->keyboardInput.update(pDemoState->keyboard);
+	}
+}
+
+void a3Client::networking(a3_DemoState* pDemoState)
+{
+
+}
+
+void a3Client::update(a3_DemoState* pDemoState)
+{
+
+}
+
+void a3Client::render(a3_DemoState* pDemoState)
+{
+	// clear color
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	if (isChatting)
+	{
+		// draw some text
+		a3textDraw(pDemoState->text,
+			// center near plane
+			0, 0.2f, -1,
+			// green
+			0, 1, 0, 1,
+			"CHATTING");
+	}
+
+	char* typingBuffer = pDemoState->keyboardInput.getTypingBuffer();
+	// draw some text
+	a3textDraw(pDemoState->text,
+		// center near plane
+		0, 0, -1,
+		// green
+		0, 1, 0, 1,
+		// typing buffer
+		"%s",
+		typingBuffer);
+}
---
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3Client.h
@@ -0,0 +1,24 @@
+#ifndef A3_CLIENT_H
+#define A3_CLIENT_H
+
+#include "a3Peer.h"
+
+class a3Client : public a3Peer
+{
+public:
+	a3Client();
+
+	void init(a3_DemoState* pDemoState) override;
+	void cleanup(a3_DemoState* pDemoState) override;
+
+	void input(a3_DemoState* pDemoState) override;
+	void networking(a3_DemoState* pDemoState) override;
+	void update(a3_DemoState* pDemoState) override;
+	void render(a3_DemoState* pDemoState) override;
+
+private:
+	// testing
+	bool isChatting = false;
+};
+
+#endif
